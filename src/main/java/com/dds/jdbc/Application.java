446065aa6101b2788d5632e630b552958f27ad04
Suber <Suber@SHZMFGWIN7WF01>
2018-05-18 16:56:20 +0800

    commit

--- src/main/java/com/dds/jdbc/Application.java
+++ src/main/java/com/dds/jdbc/Application.java
@@ -12,7 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Application implements CommandLineRunner {
 
 	private final static Logger logger = LoggerFactory.getLogger(Application.class);
