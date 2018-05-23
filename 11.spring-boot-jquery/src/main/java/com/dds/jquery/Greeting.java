b43636740e882c3277653c4c9ae4b158e4038959
Suber <Suber@SHZMFGWIN7WF01>
2018-05-23 16:03:38 +0800

    jquery

---
+++ 11.spring-boot-jquery/src/main/java/com/dds/jquery/Greeting.java
@@ -0,0 +1,21 @@
+package com.dds.jquery;
+
+public class Greeting {
+	private final Long id;
+	private final String content;
+
+	public Greeting(Long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
