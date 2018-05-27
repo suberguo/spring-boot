b58ae403a4030744cecfa925d2dc491cc081c281
T470p <suberguo@gmail.com>
2018-05-27 10:37:30 +0800

    mustache

--- 12.spring-boot-mustache/src/main/java/com/dds/mustache/WelcomeController.java
+++ 12.spring-boot-mustache/src/main/java/com/dds/mustache/WelcomeController.java
@@ -4,30 +4,30 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@ConfigurationProperties
 public class WelcomeController {
 
 	@Value("${app.welcome.title}")
-	private String title;
-	
+	private String title = "";
+
 	@Value("${app.welcome.message}")
-	private String message;
-	
+	private String message = "";
+
 	@RequestMapping("/")
-	public String welcome(Map<String,String> map) {
+	public String welcome(Map<String, String> map) {
 		map.put("title", title);
 		map.put("message", message);
 		return "welcome";
 	}
-	
+
 	@RequestMapping("/5xx")
 	public String ServiceUnavailable() {
 		throw new RuntimeException("ABC");
 	}
-	
+
 }
